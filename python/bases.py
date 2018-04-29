fd91c0dd089952641d9d243bcb9c1b7c9bb6db7a
pelegs <pelegs@gmail.com>
2018-04-30 00:21:40 +0200

    Finished, not proof-red yet

--- python/bases.py
+++ python/bases.py
@@ -15,22 +15,21 @@ d = {'0':0,
      'E':14,
      'F':15}
 
-def draw_base(num, base, k, width=1, height=1, scale=1, color='blue'):
-    L = len(str(num))
-    digits = '0'*(k-L) + str(num)
+def draw_base_normal(num, base, k, width=1, height=1, scale=1, color='blue', box_args=['thick']):
+    digits_str = k*'0' + str(num)
+    val = sum(d[digit]*base**i for i, digit in enumerate(digits_str[::-1]))
+    L = len(digits_str)
+    box_args = ','.join(box_args)
     print('\\begin{{tikzpicture}}[scale={}]'.format(scale))
-    val = int(digits, base)
-    for i, digit in enumerate(digits):
-        print('\\draw[thick] ({},0) rectangle ({},{});'.format(i*width, (i+1)*width, height))
-        print('\\node[thick] at ({},{}) {{{}}};'.format((i+.5)*width, height/2, digit))
-        print('\\node[thick] at ({},{}) {{ $\\times$ }};'.format((i+.5)*width, -.3*height))
-        print('\\node[thick] at ({},{}) {{ ${}^{{ {} }}$ }};'.format((i+.5)*width, -.75*height, base, k-i-1))
-        print('\\node[thick] at ({},{}) {{ $\\left({}\\right)$ }};'.format((i+.5)*width, -1.5*height, base**(k-i-1)))
-        print('\\draw[thick, ->] ({},{}) to ({},{});'.format((i+.5)*width, -2*height, (i+.5)*width, -3*height))
-        print('\\node[thick] at ({},{}) {{ ${}_{{10}}$ }};'.format((i+.5)*width, -3.5*height, d[digit]*base**(k-i-1)))
-        if i!=0:
-            print('\\node[] at ({},{}) {{$+$}};'.format(i*width, -3.5*height))
-        print('\\node[] at ({},{}) {{ $={}_{{10}}$ }};'.format(width*(k+.5), -3.5*height, val))
+    for i, digit in enumerate(digits_str):
+        j = L+k-i-1
+        col = color + '!' + str(int(i/L*40+5))
+        col_inv = color + '!' + str(int(j/L*40+5))
+        print('\\node[fill={}, draw, fit={{({},0)({},-1)}}, inner sep=0pt, {}] {{ ${}$ }};'.format(col_inv, 1.5*i, 1.5*(i+1), box_args, digit))
+    print('\\draw[<-, thick] (0.75, -1.25) to (0.75, -2);')
+    print('\\node[]() at (0.75, -2.5) {{ Most Significant Digit }};')
+    print('\\draw[<-, thick] ({}, -1.25) to ({}, -2);'.format(1.5*L-0.75, 1.5*L-0.75))
+    print('\\node[]() at ({}, -2.5) {{ Least Significant Digit }};'.format(1.5*L-0.75))
     print('\\end{tikzpicture}')
 
 def draw_base_fancy(num, base, k, box_args=['thick'], arrow_args=['thick'], color='blue', scale=1.0):
@@ -53,3 +52,4 @@ def draw_base_fancy(num, base, k, box_args=['thick'], arrow_args=['thick'], colo
     print('\\node[draw, fit={{({},{})({},{})}}, inner sep=0pt, very thick] {{ Total }};'.format(1.5*(L), -L-1, 1.5*(L+1), -L-2))
     print('\\node[draw, fit={{({},{})({},{})}}, inner sep=0pt, very thick] {{ $={}$ }};'.format(1.5*(L+1), -L-1, 1.5*(L+2), -L-2, val))
     print('\\end{tikzpicture}')
+
